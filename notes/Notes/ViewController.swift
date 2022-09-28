63482a2dee3605382976b677ce02b95ee0624fb5
cm1Bell <113918513+cm1Bell@users.noreply.github.com>
2022-09-28 14:26:31 -0400

    first commit

---
+++ notes/Notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Chris on 2022-09-27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //removed
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
